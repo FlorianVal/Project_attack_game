fcac3d3b4230da0c8b072c7b4070ccbccf4ded71
cbeniddir <beniddirceline@gmail.com>
2019-05-23 05:16:46 +0200

    Ajout événement clique boutons lvl 1 2 3

--- AttackGameProject/src/application/WindowController.java
+++ AttackGameProject/src/application/WindowController.java
@@ -1,7 +1,12 @@
 package application;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class WindowController {
 
@@ -12,19 +17,69 @@ public class WindowController {
     private TextField height;
 
     @FXML
-    void OpenLevelOne() {
+    void OpenLevelOne(ActionEvent event){
+    	try {
+    		
+    		Parent root = FXMLLoader.load(getClass().getResource("/application/Level1.fxml"));
+    		Scene scene = new Scene(root);
+    		Stage stage = new Stage();
+    		stage.setTitle("Level 1");
+    		stage.setScene(scene);
+    		stage.show();
+    		
+    	} catch(Exception e) {
+    		e.printStackTrace();
+    	}
+    		
+    }
+    
+    
+    
+
+    @FXML
+    void OpenLevelTwo(ActionEvent event) {
+
+    	try {
+    		
+    		Parent root = FXMLLoader.load(getClass().getResource("/application/Level2.fxml"));
+    		Scene scene = new Scene(root);
+    		Stage stage = new Stage();
+    		stage.setTitle("Level 2");
+    		stage.setScene(scene);
+    		stage.show();
+    		
+    	} catch(Exception e) {
+    		e.printStackTrace();
+    	}
     	
-
     }
-
+    
     @FXML
-    void OpenLevelThree() {
+    void OpenLevelThree(ActionEvent event) {
+    	
+    	try {
+    		
+    		Parent root = FXMLLoader.load(getClass().getResource("/application/Level3.fxml"));
+    		Scene scene = new Scene(root);
+    		Stage stage = new Stage();
+    		stage.setTitle("Level 3");
+    		stage.setScene(scene);
+    		stage.show();
+    		
+    	} catch(Exception e) {
+    		e.printStackTrace();
+    	}
 
     }
 
-    @FXML
-    void OpenLevelTwo() {
 
-    }
+    
+
+    //@Override
+    //public void initialize(URL arg0, ResourceBundle arg1) {
+//    	// TODO Auto-generated method stub
+    	
+    //}
+    
 
 }
