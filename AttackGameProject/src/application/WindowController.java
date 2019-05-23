eb26cff56ae0c555241000f92be66690cb0619e8
florian_val <florian_val@outlook.fr>
2019-05-23 14:49:59 +0100

    better check params

--- AttackGameProject/src/application/WindowController.java
+++ AttackGameProject/src/application/WindowController.java
@@ -19,71 +19,90 @@ public class WindowController {
     @FXML
     void OpenLevelOne(ActionEvent event){
     	if(CheckParameters()) {
-    	try {
-    		
-    		Parent root = FXMLLoader.load(getClass().getResource("/application/Level1.fxml"));
-    		Scene scene = new Scene(root);
-    		Stage stage = new Stage();
-    		stage.setTitle("Level 1");
-    		stage.setScene(scene);
-    		stage.show();
-    		
-    	} catch(Exception e) {
-    		e.printStackTrace();
-    	}
+	    	try {
+	    		
+	    		Parent root = FXMLLoader.load(getClass().getResource("/application/Level1.fxml"));
+	    		Scene scene = new Scene(root);
+	    		Stage stage = new Stage();
+	    		stage.setTitle("Level 1");
+	    		stage.setScene(scene);
+	    		stage.show();
+	    		
+	    	} catch(Exception e) {
+	    		e.printStackTrace();
+	    	}
     	}
     	else {
-    		System.out.print("incorrect params");
+    		// TODO show message on window
+    		System.out.println("incorrect params");
     	}
     		
     }
  
     @FXML
     void OpenLevelTwo(ActionEvent event) {
-
-    	try {
-    		
-    		Parent root = FXMLLoader.load(getClass().getResource("/application/Level2.fxml"));
-    		Scene scene = new Scene(root);
-    		Stage stage = new Stage();
-    		stage.setTitle("Level 2");
-    		stage.setScene(scene);
-    		stage.show();
-    		
-    	} catch(Exception e) {
-    		e.printStackTrace();
+    	if(CheckParameters()) {
+	    	try {
+	    		
+	    		Parent root = FXMLLoader.load(getClass().getResource("/application/Level2.fxml"));
+	    		Scene scene = new Scene(root);
+	    		Stage stage = new Stage();
+	    		stage.setTitle("Level 2");
+	    		stage.setScene(scene);
+	    		stage.show();
+	    		
+	    	} catch(Exception e) {
+	    		e.printStackTrace();
+	    	}
+    	}
+    	else {
+    		// TODO show message on window
+    		System.out.println("incorrect params");
     	}
     	
     }
     
     @FXML
     void OpenLevelThree(ActionEvent event) {
-    	
-    	try {
-    		
-    		Parent root = FXMLLoader.load(getClass().getResource("/application/Level3.fxml"));
-    		Scene scene = new Scene(root);
-    		Stage stage = new Stage();
-    		stage.setTitle("Level 3");
-    		stage.setScene(scene);
-    		stage.show();
-    		
-    	} catch(Exception e) {
-    		e.printStackTrace();
+    	if(CheckParameters()) {
+	    	try {
+	    		
+	    		Parent root = FXMLLoader.load(getClass().getResource("/application/Level3.fxml"));
+	    		Scene scene = new Scene(root);
+	    		Stage stage = new Stage();
+	    		stage.setTitle("Level 3");
+	    		stage.setScene(scene);
+	    		stage.show();
+	    		
+	    	} catch(Exception e) {
+	    		e.printStackTrace();
+	    	}
+    	}
+    	else {
+    		// TODO show message on window
+    		System.out.println("incorrect params");
     	}
 
     }
     boolean CheckParameters() {
+    	//check if field not empty
     	if((width.getText() == null || width.getText().trim().isEmpty()) || ((height.getText() == null || height.getText().trim().isEmpty()))) {
     		return false;
     	}
     	else {
-	    	if(Integer.parseInt(width.getText()) >= 80 && Integer.parseInt(height.getText()) >= 50) {
-	    		return true;
-	    	}
-	    	else {
-	    		return false;
-	    	}
+    		// if not empty check value in field (if int(try catch) and if size >(80,50))
+    		try {
+		    	if(Integer.parseInt(width.getText()) >= 80 && Integer.parseInt(height.getText()) >= 50) {
+		    		return true;
+		    	}
+		    	else {
+		    		return false;
+		    	}
+    		}
+    		catch(NumberFormatException e) {
+    			System.out.println("Problem converting to Int");
+    			return false;
+    		}
     	}
     }
     
