41beccebe23ae62610526e564dd8927d0846009c
cbeniddir <beniddirceline@gmail.com>
2019-07-09 03:14:16 +0200

    Classe Element

--- src/application/ElementClass.java
+++ src/application/ElementClass.java
@@ -1,10 +1,20 @@
 package application;
 
 public class ElementClass {
-	Element element;
 	
-	public ElementClass(Element element ){
+	Element element;
+	private int counter = 0;
+	
+	public ElementClass(Element element, int counter){
 		this.element = element;
+	}
+	
+	public int getCounter(){
+		return counter;
+	}
+	
+	public void setCounter(int counter2){
+		counter = counter2;
 	}
 	
 }
